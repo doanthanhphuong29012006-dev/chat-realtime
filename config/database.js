8c05206f708333ad47c6f0670a822f38059f2225
phuongne <doanthanhphuong29012006@gmail.com>
2026-07-25 19:42:16 +0700

    Database connect

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 module.exports.connect = async () => {
     try {
-    await mongoose.connect('mongodb://127.0.0.1:27017/chat-realtime');
+    await mongoose.connect(process.env.MONGO_URL);
     console.log("Successfully connected to the database!")
     } catch (error) {
     console.log("Database connection failed");
