113429f04708d17e8d87bf92ab494d70328c996d
phuongne <doanthanhphuong29012006@gmail.com>
2026-07-25 15:48:01 +0700

    Display basic chat

--- controllers/chat.controller.js
+++ controllers/chat.controller.js
@@ -1,10 +1,35 @@
+const Chat = require('../models/chat.model');
+const User = require('../models/user.model');
+
 // [GET]/chat
-module.exports.index = (req, res) => {
-    _io.on('connection', (socket) => {
-        console.log('a user connected', socket.id);
+module.exports.index = async (req, res) => {
+    const userId = res.locals.user.id;
+
+    _io.once('connection', (socket) => {
+        socket.on("CLIENT_SEND_MESSAGE", async (content) => {
+            const chat = new Chat({
+                user_id: userId,
+                content: content
+            });
+
+            await chat.save();
+        });
     });
 
+    const chats = await Chat.find({
+        deleted: false
+    });
+
+    for (const chat of chats) {
+        const userInfo = await User.findOne({
+            _id: chat.user_id
+        }).select("fullName");
+
+        chat.userInfo = userInfo;
+    }
+
     res.render('pages/chat/index', {
-        pageTitle: "Chat"
+        pageTitle: "Chat",
+        chats: chats
     });
 }
