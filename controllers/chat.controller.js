0b8669ffe20002c286d029e3ffea44c399e3b08e
phuongne <doanthanhphuong29012006@gmail.com>
2026-07-25 16:50:38 +0700

    Display real-time chat

--- controllers/chat.controller.js
+++ controllers/chat.controller.js
@@ -4,6 +4,7 @@ const User = require('../models/user.model');
 // [GET]/chat
 module.exports.index = async (req, res) => {
     const userId = res.locals.user.id;
+    const fullName = res.locals.user.fullName;
 
     _io.once('connection', (socket) => {
         socket.on("CLIENT_SEND_MESSAGE", async (content) => {
@@ -13,6 +14,12 @@ module.exports.index = async (req, res) => {
             });
 
             await chat.save();
+
+            _io.emit("SERVER_RETURN_MESSAGE", {
+                userId: userId,
+                fullName: fullName,
+                content: content
+            });
         });
     });
 
