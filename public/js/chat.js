f7d7f06da9b500e77c57a4d23f796cad362ff2ea
phuongne <doanthanhphuong29012006@gmail.com>
2026-07-26 08:18:50 +0700

    Typing feature

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,6 +1,8 @@
 import * as Popper from 'https://cdn.jsdelivr.net/npm/@popperjs/core@^2/dist/esm/index.js'
 
 // CLIENT_SEND_MESSAGE
+var timeOut;
+
 const formSendData = document.querySelector(".chat .inner-form");
 if (formSendData) {
     formSendData.addEventListener("submit", (e) => {
@@ -9,6 +11,8 @@ if (formSendData) {
         if (content) {
             socket.emit("CLIENT_SEND_MESSAGE", content);
             e.target.elements.content.value = "";
+            socket.emit("CLIENT_SEND_TYPING", "hidden");
+            clearTimeout(timeOut);
         }
     });
 }
@@ -18,6 +22,7 @@ if (formSendData) {
 socket.on("SERVER_RETURN_MESSAGE", (data) => {
     const myId = document.querySelector("[my-id]").getAttribute("my-id");
     const body = document.querySelector(".chat .inner-body");
+    const boxTyping = document.querySelector(".chat .inner-list-typing");
 
     const div = document.createElement("div");
     let htmlFullname = "";
@@ -34,7 +39,7 @@ socket.on("SERVER_RETURN_MESSAGE", (data) => {
         <div class="inner-content">${data.content}</div>
     `;
 
-    body.appendChild(div);
+    body.insertBefore(div, boxTyping);
 
     body.scrollTop = body.scrollHeight;
 });
@@ -46,6 +51,18 @@ if (bodyChat) {
     bodyChat.scrollTop = bodyChat.scrollHeight;
 }
 // Scroll Chat To Bottom
+
+// Show Typing
+const showTyping = () => {
+    socket.emit("CLIENT_SEND_TYPING", "show");
+
+    clearTimeout(timeOut);
+
+    timeOut = setTimeout(() => {
+        socket.emit("CLIENT_SEND_TYPING", "hidden");
+    }, 3000);
+}
+// End Show Typing
 
 // Show Icon Chat
 const buttonIcon = document.querySelector('.button-icon');
@@ -76,7 +93,63 @@ if (emojiPicker) {
     emojiPicker.addEventListener("emoji-click", (e) => {
         const icon = e.detail.unicode;
         inputChat.value = inputChat.value + icon;
+
+        const endChat = inputChat.value.length;
+        inputChat.setSelectionRange(endChat, endChat);
+        inputChat.focus();
+
+        showTyping();
     });
 }
 // End Insert Icon To Input
 // End Icon Chat
+
+// Input Keyup
+const inputContent = document.querySelector(".chat .inner-form input[name='content']");
+if (inputContent) {
+    inputContent.addEventListener("keyup", (e) => {
+        if (e.key === "Enter") {
+            return;
+        }
+
+        showTyping();
+    });
+}
+//End Input Keyup
+
+// SERVER_RETURN_TYPING
+const elementListTyping = document.querySelector(".chat .inner-list-typing");
+if (elementListTyping) {
+    socket.on("SERVER_RETURN_TYPING", (data) => {
+        if (data.type == "show") {
+            const existTyping = elementListTyping.querySelector(`[user-id="${data.userId}"]`);
+            if (!existTyping) {
+                const boxTyping = document.createElement("div");
+                boxTyping.classList.add("box-typing");
+                boxTyping.setAttribute("user-id", data.userId);
+
+                boxTyping.innerHTML = `
+                    <div class="inner-name">${data.fullName}</div>
+                    <div class="inner-dots">
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                    </div>
+                `;
+
+                elementListTyping.appendChild(boxTyping);
+
+                const bodyChat = document.querySelector(".chat .inner-body");
+                if(bodyChat) {
+                    bodyChat.scrollTop = bodyChat.scrollHeight;
+                }
+            }
+        } else {
+            const boxTypingRemove = elementListTyping.querySelector(`[user-id="${data.userId}"]`);
+            if (boxTypingRemove) {
+                elementListTyping.removeChild(boxTypingRemove);
+            }
+        }
+    });
+}
+// End SERVER_RETURN_TYPING
