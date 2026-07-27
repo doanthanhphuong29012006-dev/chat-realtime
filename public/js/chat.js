332b39bf7d9e69010d62894ca49fae8b072ccb94
phuongne <doanthanhphuong29012006@gmail.com>
2026-07-27 09:57:49 +0700

    Display image in full screen

--- public/js/chat.js
+++ public/js/chat.js
@@ -85,6 +85,9 @@ socket.on("SERVER_RETURN_MESSAGE", (data) => {
     body.insertBefore(div, boxTyping);
 
     body.scrollTop = body.scrollHeight;
+
+    //Preview Images
+    const gallery = new Viewer(div);
 });
 // End SERVER_RETURN_MESSAGE
 
@@ -226,3 +229,10 @@ if (elementListTyping) {
     });
 }
 // End SERVER_RETURN_TYPING
+
+// Preview Full Image
+const bodyChatPreviewImage = document.querySelector(".chat .inner-body");
+if (bodyChatPreviewImage) {
+    const gallery = new Viewer(bodyChatPreviewImage);
+}
+// End Preview Full Image
