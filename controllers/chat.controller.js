87a8558069e8f6b4e3f6601b5e0d837158a8e93e
phuongne <doanthanhphuong29012006@gmail.com>
2026-07-27 09:33:48 +0700

    Split Socket.io files

--- controllers/chat.controller.js
+++ controllers/chat.controller.js
@@ -1,49 +1,11 @@
 const Chat = require('../models/chat.model');
 const User = require('../models/user.model');
 
-const uploadToCloudinaryHelper = require('../helpers/uploadToCloudinary.js');
+const chatSocket = require('../sockets/chat.socket');
 
 // [GET]/chat
 module.exports.index = async (req, res) => {
-    const userId = res.locals.user.id;
-    const fullName = res.locals.user.fullName;
-
-    _io.once('connection', (socket) => {
-        socket.on("CLIENT_SEND_MESSAGE", async (data) => {
-            let images = [];
-            for (const imageBuffer of data.images) {
-                const link = await uploadToCloudinaryHelper(imageBuffer);
-                images.push(link);
-            }
-
-            console.log(images);
-            
-            const chat = new Chat({
-                user_id: userId,
-                content: data.content,
-                images: images
-            });
-
-            await chat.save();
-
-            _io.emit("SERVER_RETURN_MESSAGE", {
-                userId: userId,
-                fullName: fullName,
-                content: data.content,
-                images: images
-            });
-        });
-
-        // Typing
-        socket.on("CLIENT_SEND_TYPING", async (type) => {
-            socket.broadcast.emit("SERVER_RETURN_TYPING", {
-                userId: userId,
-                fullName: fullName,
-                type: type
-            });
-        });
-        // End Typing
-    });
+    chatSocket(res);
 
     const chats = await Chat.find({
         deleted: false
---
+++ sockets/chat.socket.js
@@ -0,0 +1,45 @@
+const Chat = require('../models/chat.model');
+
+const uploadToCloudinaryHelper = require('../helpers/uploadToCloudinary.js');
+
+module.exports = (res) => {
+    const userId = res.locals.user.id;
+    const fullName = res.locals.user.fullName;
+
+    _io.once('connection', (socket) => {
+        socket.on("CLIENT_SEND_MESSAGE", async (data) => {
+            let images = [];
+            for (const imageBuffer of data.images) {
+                const link = await uploadToCloudinaryHelper(imageBuffer);
+                images.push(link);
+            }
+
+            console.log(images);
+            
+            const chat = new Chat({
+                user_id: userId,
+                content: data.content,
+                images: images
+            });
+
+            await chat.save();
+
+            _io.emit("SERVER_RETURN_MESSAGE", {
+                userId: userId,
+                fullName: fullName,
+                content: data.content,
+                images: images
+            });
+        });
+
+        // Typing
+        socket.on("CLIENT_SEND_TYPING", async (type) => {
+            socket.broadcast.emit("SERVER_RETURN_TYPING", {
+                userId: userId,
+                fullName: fullName,
+                type: type
+            });
+        });
+        // End Typing
+    });
+}
