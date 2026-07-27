6119c70e6dfb05ab063a47acb6a093345767efcd
phuongne <doanthanhphuong29012006@gmail.com>
2026-07-27 13:40:35 +0700

    Home page

--- routes/index.route.js
+++ routes/index.route.js
@@ -1,9 +1,12 @@
 const authRouter = require('./auth.route');
 const chatRouter = require('./chat.route');
+const homeRouter = require('./home.route');
 
 const authMiddleware = require('../middlewares/auth.middleware');
 
 module.exports = (app) => {
+    app.use('/', authMiddleware.requireAuth, homeRouter);
+
     app.use('/auth', authRouter);
 
     app.use('/chat', authMiddleware.requireAuth, chatRouter);
