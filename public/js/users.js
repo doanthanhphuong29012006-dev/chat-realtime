b974ebd965a700faed3f65a0459b05ceca824eff
phuongne <doanthanhphuong29012006@gmail.com>
2026-07-29 14:30:08 +0700

    Send real-time friend requests

--- public/js/users.js
+++ public/js/users.js
@@ -57,3 +57,15 @@ if (listBtnAcceptFriend.length > 0) {
     });
 }
 // End Friend Request Acceptance Feature
+
+// SERVER_RETURN_LENGTH_ACCEPT_FRIEND
+const badgeUserAccept = document.querySelector("[badge-users-accept]");
+if (badgeUserAccept) {
+    const userId = badgeUserAccept.getAttribute("badge-users-accept");
+    socket.on("SERVER_RETURN_LENGTH_ACCEPT_FRIEND", (data) => {
+        if (userId === data.userId) {
+            badgeUserAccept.innerHTML = data.lengthAcceptFriend;
+        }
+    });
+}
+// End SERVER_RETURN_LENGTH_ACCEPT_FRIEND
--- sockets/users.socket.js
+++ sockets/users.socket.js
@@ -31,6 +31,16 @@ module.exports = (res) => {
                     $push: { requestFriends: userId }
                 });
             }
+
+            const infoUserB = await User.findOne({
+                _id: userId
+            });
+            const lengthAcceptFriends = infoUserB.acceptFriends.length;
+
+            socket.broadcast.emit("SERVER_RETURN_LENGTH_ACCEPT_FRIEND", {
+                userId: userId,
+                lengthAcceptFriend: lengthAcceptFriends
+            });
         });
 
         // Friend Request Cancellation Feature
